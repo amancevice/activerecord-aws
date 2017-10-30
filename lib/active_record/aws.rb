d0fd3cf5a4fed17793f9220879136e9f9a7d37a2
Alexander Mancevice <amancevice@cargometrics.com>
2017-10-30 14:46:48 -0400

    Separate migration from decryption

--- lib/active_record/aws.rb
+++ lib/active_record/aws.rb
@@ -4,13 +4,9 @@ module ActiveRecord
   class Base
     class << self
       def decrypt_password
-        Migration.stage :DecryptPassword do |stage|
-          stage.step :kms_decrypt, :ciphertext do
-            blob      = Base64.decode64 connection_config[:ciphertext]
-            decrypted = ::Aws::KMS::Client.new.decrypt ciphertext_blob:blob
-            connection_config.update password:decrypted[:plaintext]
-          end
-        end
+        blob      = Base64.decode64 connection_config[:ciphertext]
+        decrypted = ::Aws::KMS::Client.new.decrypt ciphertext_blob:blob
+        connection_config.update password:decrypted[:plaintext]
       end
 
       def encrypted_password?
@@ -21,6 +17,14 @@ module ActiveRecord
 
   class Migration
     class << self
+      def decrypt_password
+        stage :DecryptPassword do |stage|
+          stage.step :kms_decrypt, :ciphertext do
+            Base.decrypt_password
+          end
+        end
+      end
+
       def stage name
         start = Time.now.utc
         puts "== #{start.strftime('%Y%m%d%H%M%S')} #{name}: begin ".ljust 79, '='
--- lib/active_record/aws/version.rb
+++ lib/active_record/aws/version.rb
@@ -1,5 +1,5 @@
 module ActiveRecord
   module Aws
-    VERSION = "0.1.0"
+    VERSION = "0.1.1"
   end
 end
